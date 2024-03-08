433d7497bd148c94cab6bcfc0219b8f66f18791b
Esteban Cortina <estebannsebastian@gmail.com>
2024-03-08 13:51:11 -0600

    Add routes refactor

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
 
-const sign_in_controller = require("../controllers/sign_in.js");
-router.get("/", sign_in_controller);
+router.get("/", (req, res) => {
+  res.status(200).send(
+    JSON.stringify({
+      data: "Software seguro practica 1 api",
+    })
+  );
+});
+
+const sign_in_router = require("./sign_in.js");
+router.use("/sign-in", sign_in_router);
 
 module.exports = router;
-
