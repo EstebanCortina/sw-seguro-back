379b093692973075b326dc573c319c870baebe0a
Esteban Cortina <estebannsebastian@gmail.com>
2024-03-08 13:52:22 -0600

    Add cors policy in app

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const { HOST, PORT } = require("./config/index.js");
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
+
+app.use(morgan("common"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PATCH",
+    credentials: true,
+  })
+);
 
 const router = require("./routes");
 app.use("/", router);
@@ -8,4 +22,3 @@ app.use("/", router);
 app.listen(PORT, HOST, () => {
   console.log(`Running on ${HOST}:${PORT}`);
 });
-
