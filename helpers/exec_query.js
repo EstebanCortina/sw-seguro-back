6128a408fd2079d0eedc9365eb43cb5753a7a6d0
Esteban Cortina <estebannsebastian@gmail.com>
2024-03-08 15:41:02 -0600

    Add paramas for query preparation from object properties

--- helpers/exec_query.js
+++ helpers/exec_query.js
@@ -1,13 +1,13 @@
 //Este helper lo que hace es obtener una conexión, y ejecutar la query de forma controlada.
 const pool = require("../config/mysql.js");
-function exec_query(query) {
+function exec_query(query, params) {
   return new Promise((resolve, reject) => {
     pool.getConnection((err, connection) => {
       if (err) {
         resolve("Error in pool connection:" + err);
       }
       //el parametro query necesita llegar sanitizado o sanitizarse antes de lanzarse
-      connection.query(query, (err, results, fields) => {
+      connection.query(query, params, (err, results, fields) => {
         if (err) {
           connection.release();
           reject("Error in query execution:" + err);
