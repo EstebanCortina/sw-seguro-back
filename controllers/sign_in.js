60048780b958f215e74d1490c56c5fd725376d24
Esteban Cortina <estebannsebastian@gmail.com>
2024-03-08 13:51:53 -0600

    Add sign-in controller refactor

--- controllers/sign_in.js
+++ controllers/sign_in.js
@@ -1,5 +1,10 @@
 const exec_query = require("../helpers/exec_query.js");
 module.exports = (req, res) => {
-  let results = exec_query("SELECT * FROM users");
-  res.status(200).send(results);
+  exec_query("SELECT * FROM users")
+    .then((results) => {
+      res.status(200).send(results);
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    });
 };
