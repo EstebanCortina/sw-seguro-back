ab589e91b06cf87488cf135177a531947f924380
Esteban Cortina <estebannsebastian@gmail.com>
2024-03-08 11:17:13 -0600

    Add mysql2 configuration

--- config/mysql.js
+++ config/mysql.js
@@ -1,21 +1,24 @@
-const {
-  MYSQL_HOST,
-  MYSQL_PORT,
-  MYSQL_USER,
-  MYSQL_PASSWORD,
-  MYSQL_DATABASE,
-} = require("../config/mysql.js");
 const { createPool } = require("mysql2");
 
 const pool = createPool({
-  host: MYSQL_HOST,
-  port: MYSQL_PORT,
-  user: MYSQL_USER,
-  password: MYSQL_PASSWORD,
-  database: MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST || "127.0.0.1",
+  port: process.env.MYSQL_PORT || 3309,
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "1234",
+  database: process.env.MYSQL_DATABASE || "sw_dev",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+});
+
+process.on("SIGINT", () => {
+  pool.end((err) => {
+    if (err) {
+      console.error("Error in pool close connections:", err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
 });
 
 pool.on("error", (err) => {
