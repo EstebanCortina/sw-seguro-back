41f8ab21bb8103dae6bf812b24932b8636a106b0
Esteban Cortina <estebannsebastian@gmail.com>
2024-03-10 01:29:54 -0600

    Add sign_in middlewares

--- routes/sign_in.js
+++ routes/sign_in.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const clean_body = require("../middlewares/clean_body.js");
+const random_delay = require("../middlewares/random_delay.js");
 
 const sign_in_controller = require("../controllers/sign_in.js");
-router.post("/", clean_body, sign_in_controller);
+router.post("/", random_delay, clean_body, sign_in_controller);
 
 module.exports = router;
 
