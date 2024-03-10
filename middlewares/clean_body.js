d82106e27db090dd7e22f60f147f62bc1d0482e7
Esteban Cortina <estebannsebastian@gmail.com>
2024-03-10 01:28:25 -0600

    Update clean body middleware for input data validations with refactor

--- middlewares/clean_body.js
+++ middlewares/clean_body.js
@@ -1,15 +1,33 @@
+const validator = require("validator");
 function clean_body(req, res, next) {
+  let err = null;
+  const regex = /^[a-zA-Z0-9._-]+@ucol\.mx$/;
   Object.keys(req.body).forEach((field) => {
-    if (req.body[field].length < 80) {
-      req.body[field] = req.body[field].trim().toLowerCase();
+    if (req.body[field].length < 80 && typeof req.body[field] === "string") {
+      if (field === "email") {
+        if (
+          !validator.isEmail(req.body[field]) ||
+          !regex.test(req.body[field])
+        ) {
+          err = res.status(400).send({
+            httpStatus: 400,
+            message: "Bad request",
+            data: [],
+          });
+        }
+        req.body[field] = req.body[field].trim().toLowerCase();
+      }
     } else {
-      res.status(400).send({
+      err = res.status(400).send({
         httpStatus: 400,
-        message: "Bad body request",
-        data: `'${field}' too long content`,
+        message: "Bad request",
+        data: [],
       });
     }
   });
+  if (err) {
+    return err;
+  }
   next();
 }
 
