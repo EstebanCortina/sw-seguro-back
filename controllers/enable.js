04da48399172e195f51498bd2dbeef1cb267b9ea
Esteban Cortina <estebannsebastian@gmail.com>
2024-04-18 01:46:53 -0600

    Fix enable and disable

--- controllers/enable.js
+++ controllers/enable.js
@@ -1,7 +1,7 @@
 const User = require("../models/user_model.js");
 module.exports = async (req, res) => {
+  let response = null;
   if (req.session.user_type_name === "profesor") {
-    let response = null;
     try {
       response = await User.set_user_is_active(req.params.id, 1);
       res.status(response.httpStatus);
